e4e44e81d01a130fb406678a615fd5d59256e112
Atanas V. Dobrev <atanas.vasilev.dobrev@gmail.com>
2017-12-07 18:09:05 +0200

    Starting

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="default.css">
+		<meta charset="utf-8">
+		<title>Liverpool FC - WebStore</title>
+	</head>
+	<body>
+		<div class="centering">
+			<h1>Welcome to Official Liverpool FC Store!</h1>
+			<form method="post" action="index.html">
+				<input placeholder="Search" name="search">
+				<input type="submit" value="Search">
+			</form>
+		</div>
+		<?php
+			if(strlen($_POST['search']) != 2)
+			{
+				echo '<br>'.$_POST['search'];
+			}
+		?>
+	</body>
+</html>
